6de86da8bc11d45e1cb7d92c0125522d2a39c2ae
Adam Zálešák <xzalesak@fi.muni.cz>
2021-10-21 23:17:38 +0200

    Change API URL

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,19 +1,50 @@
 export const users = [
-    {id: 1, conversations: [], email: "adam", name: "Adam", password: "", sentMessages: []},
-    {id: 2, conversations: [], email: "anna", name: "Anna", password: "", sentMessages: []},
-    {id: 3, conversations: [], email: "jakub", name: "Jakub", password: "", sentMessages: []},
-    {id: 4, conversations: [], email: "aneta", name: "Aneta", password: "", sentMessages: []}
+  {
+    id: 1,
+    conversations: [],
+    email: "adam",
+    name: "Adam",
+    password: "",
+    sentMessages: [],
+  },
+  {
+    id: 2,
+    conversations: [],
+    email: "anna",
+    name: "Anna",
+    password: "",
+    sentMessages: [],
+  },
+  {
+    id: 3,
+    conversations: [],
+    email: "jakub",
+    name: "Jakub",
+    password: "",
+    sentMessages: [],
+  },
+  {
+    id: 4,
+    conversations: [],
+    email: "aneta",
+    name: "Aneta",
+    password: "",
+    sentMessages: [],
+  },
 ];
 
-export const apiUrl = "http://server.adamzalesak.cz:5000";
+export const apiUrl = "http://azmessenger-api.herokuapp.com";
 
 export const loggedUserId = (userName: string): number => {
-    return (
-        (userName.startsWith("/adam")) ? 1
-        : (userName === "/anna") ? 2
-        : (userName === "/jakub") ? 3
-        : (userName === "/david") ? 4
-        : (userName === "/aneta") ? 5
-        : 0
-    );
+  return userName.startsWith("/adam")
+    ? 1
+    : userName === "/anna"
+    ? 2
+    : userName === "/jakub"
+    ? 3
+    : userName === "/david"
+    ? 4
+    : userName === "/aneta"
+    ? 5
+    : 0;
 };
